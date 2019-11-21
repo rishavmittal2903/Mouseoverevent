a4372f32e9f5cb439d4c9fcb5649eadb31677f49
Rishav Mittal <rishav.mittal@nagarro.com>
2019-11-21 17:34:09 +0530

    Code added to get total length of free hand drawing during cursor movement

---
+++ FreeHandCanvass/Main.js
@@ -0,0 +1,79 @@
+var canvas, ctx, flag = false,
+prevX = 0,
+currX = 0,
+prevY = 0,
+currY = 0,
+dot_flag = false;
+totalLength=0;
+var x = "black",
+y = 2;
+
+function init() {
+canvas = document.getElementById('can');
+ctx = canvas.getContext("2d");
+w = canvas.width;
+h = canvas.height;
+
+canvas.addEventListener("mousemove", function (e) {
+    findxy('move', e)
+}, false);
+canvas.addEventListener("mousedown", function (e) {
+    findxy('down', e)
+}, false);
+canvas.addEventListener("mouseup", function (e) {
+    findxy('up', e)
+}, false);
+canvas.addEventListener("mouseout", function (e) {
+    findxy('out', e)
+}, false);
+}
+function draw() {
+ctx.beginPath();
+ctx.moveTo(prevX, prevY);
+ctx.lineTo(currX, currY);
+ctx.strokeStyle = x;
+ctx.lineWidth = y;
+ctx.stroke();
+ctx.closePath();
+}
+function erase()
+{
+    document.getElementById("objectLength").innerText="";
+    ctx.clearRect(0, 0, w, h);
+}
+function findxy(res, e) {
+if (res == 'down') {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - canvas.offsetLeft;
+    currY = e.clientY - canvas.offsetTop;
+
+    flag = true;
+    dot_flag = true;
+    if (dot_flag) {
+        ctx.beginPath();
+        ctx.fillStyle = x;
+        ctx.fillRect(currX, currY, 2, 2);
+        ctx.closePath();
+        dot_flag = false;
+    }
+}
+if (res == 'up' || res == "out") {
+    flag = false;
+}
+if (res == 'move') {
+    if (flag) {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+        totalLength+=currX;
+        document.getElementById("objectLength").innerText=totalLength;
+        draw();
+    }
+    else
+    {
+        totalLength=0;
+    }
+}
+}
